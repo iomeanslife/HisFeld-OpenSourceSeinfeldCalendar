7e8a33504a281f2533281693c89eb416facca36d
Salem5 <salem5@outlook.com>
2014-08-25 17:09:16 +0100

    Initial upload to github.

---
+++ Model/Chain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace HisFeldLibrary.Model
+{
+    [DataContract]
+    public class Chain : ModelBase
+    {
+        [DataMember]
+        private DateTime start;
+
+
+        public DateTime Start
+        {
+            get { return start; }
+            set
+            {
+                start = value;
+                RaisePropertyChanged("Start");
+            }
+        }
+
+        [DataMember]
+        private DateTime end;
+
+        public DateTime End
+        {
+            get { return end; }
+            set
+            {
+                end = value;
+                RaisePropertyChanged("End");
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return Convert.ToInt32((End - Start).TotalDays + 1);
+            }
+        }
+        
+      
+    }
+}
---
+++ Model/MonthPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HisFeldLibrary.Model
+{
+    [DataContract]
+    public class MonthPage : ModelBase
+    {
+       
+      
+        [DataMember]
+        private bool[] hits;
+
+        public bool[] Hits
+        {
+            get { return hits; }
+            set
+            {
+                hits = value;
+                RaisePropertyChanged("Hits");
+            }
+        }
+
+        [DataMember]
+        private DateTime[] daysDates;
+
+        public DateTime[] DaysDates
+        {
+            get { return daysDates; }
+            set
+            {
+                daysDates = value;
+
+            }
+        }
+
+        public void UpdateBindings(string propertyName)
+        {
+            RaisePropertyChanged(propertyName);
+        }
+    }
+}
